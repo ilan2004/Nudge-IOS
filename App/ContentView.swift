f0ec0ee7e531f3b565c49ff48209c7a1f888d35e
Ilan Usman <ilanusman03@gmail.com>
2025-10-23 01:40:54 +0530

    Update files

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 import UIKit
-import Features.Friends
 
 // MARK: - Shapes
 struct TopRoundedRectangle: Shape {
@@ -347,64 +346,6 @@ struct ContentView: View {
                     .padding(.horizontal, 20)
                     .padding(.bottom, 20)
                 }
-            }
-        }
-    }
-}
-
-// MARK: - Friends Feature
-
-struct FriendsView: View {
-    @State private var showDetailOverlay = false
-    @State private var selectedFriend: Friend?
-    @EnvironmentObject private var personalityManager: PersonalityManager
-    
-    private var friends: [Friend] {
-        Friend.mockFriends.sorted { $0.rank < $1.rank }
-    }
-    
-    var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                // Header
-                HStack {
-                    Text("Friends")
-                        .font(.custom("Tanker-Regular", size: 28))
-                        .fontWeight(.bold)
-                        .padding(.horizontal)
-                    
-                    Spacer()
-                    
-                    // Add Friend Button
-                    Button(action: {}) {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.title2)
-                            .foregroundColor(personalityManager.personalityColors.primary)
-                    }
-                    .padding(.trailing)
-                }
-                .padding(.top, 8)
-                .padding(.bottom, 16)
-                
-                // Friends List
-                VStack(spacing: 16) {
-                    ForEach(friends) { friend in
-                        FriendCard(friend: friend) {
-                            selectedFriend = friend
-                            showDetailOverlay = true
-                        }
-                        .padding(.horizontal)
-                    }
-                }
-                .padding(.bottom, 24)
-            }
-        }
-        .frame(maxWidth: 440)
-        .sheet(isPresented: $showDetailOverlay) {
-            if let friend = selectedFriend {
-                FriendDetailOverlay(friend: friend)
-                    .presentationDetents([.medium, .large])
-                    .presentationDragIndicator(.visible)
             }
         }
     }
